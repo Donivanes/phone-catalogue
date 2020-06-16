f7880c4e0ca87bf44b3790cedf991de2dce99b2a
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-06-16 11:56:34 +0200

    back done

---
+++ server/routes/phones.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const phonesData = require("../phones.json");
+
+router.get("/", (req, res) => {
+  res.status(200).json(phonesData);
+});
+
+module.exports = router;
